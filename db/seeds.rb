94def08b947560ceeb51f1db6d455beb0c7b4f51
Elizabeth Fitzpatrick <elizabethfitzpatrick16@gmail.com>
2016-09-24 14:53:23 -0400

    create seeds for food scraps

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+client = SODA::Client.new({:domain => "data.cityofnewyork.us", :app_token => ENV['DISPOSAL_NETWORKS']})
+
+results = client.get("9m2c-n6wx", :$limit => 5000)
+
+materials = ["food", "textiles", "clothing", "electronics"]
+
+materials.each do |m|
+  MaterialType.create(name: m)
+end
+
+results.each do |result|
+  location = Location.new(latitude: result["location_1"]["coordinates"][0], longitude: result["location_1"]["coordinates"][0])
+  location.material_type << MaterialType.where(name: "food")
+end
